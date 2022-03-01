f073e4a229f496f6f095fcb2b1cb27c5fe57b74a
wasimakram11 <wa426m@gmail.com>
2022-03-01 09:55:26 +0500

    1st

---
+++ app/src/main/java/com/example/dhobi/Spleash.java
@@ -0,0 +1,14 @@
+package com.example.dhobi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Spleash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spleash);
+    }
+}
